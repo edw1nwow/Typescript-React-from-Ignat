8d30ea12ac5d47c7835c7d1748bd65127c4d43ac
edw1nwow <33133870+edw1nwow@users.noreply.github.com>
2020-11-16 22:37:39 +0200

    Hot fix

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -19,13 +19,9 @@ function Routes() {
             {/*в начале мы попадаем на страницу "/" и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после "/" ничего не будет)*/}
             <Route path={"/"} exact render={() => <Redirect to={"PRE_JUNIOR"}/>}/>
-
             <Route path={'/PRE_JUNIOR'} render={() => <PreJunior/>}/>
             <Route path={'/JUNIOR'} render={() => <Junior/>}/>
             <Route path={'/JUNIOR_PLUS'} render={() => <JuniorPlus/>}/>
-                /JUNIOR
-
-                // add routes
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
             <Route render={() => <Error404/>}/>
